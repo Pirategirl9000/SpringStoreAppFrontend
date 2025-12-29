0488e46fd6c010ea167eaa62f83f78e8253ffe89
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-12-28 20:26:20 -0600

    Added basic About page

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
         <div>
-
+            <h1>About Us</h1>
+            <p>We are company, we sell product. You buy our product maybe?</p>
         </div>
     )
 }
