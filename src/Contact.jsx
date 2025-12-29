32b8c080314bf473efeb1de37b29a21dbcaa759a
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-12-28 20:25:03 -0600

    Added basic contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,9 @@
 export default function Contact() {
     return (
         <div>
-
+            <h1>Contact Us!</h1>
+            <p>Email: companyproducts@gmail.com</p>
+            <p>Call us: 123-456-7890</p>
         </div>
     )
 }
