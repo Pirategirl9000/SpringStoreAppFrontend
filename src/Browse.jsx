2dbecd165c156b8e530c5905e768481417d147b8
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-01-01 15:23:03 -0600

    Checklist of categories now renders and defers things to a handleSubmit method which will update the screen with a post request

--- src/Browse.jsx
+++ src/Browse.jsx
@@ -1,7 +1,57 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 export default function Browse() {
+
     return (
         <div>
-
+            <CategoryChecklist />
         </div>
     )
+}
+
+function CategoryChecklist() {
+    const categories = useCategories();
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {categories.map(cat => {
+                const category = cat.category;
+
+                // Categories are originally all caps so we convert it to a more UI friendly version
+                const categoryLabel = category.charAt(0) + category.substring(1, category.length).toLowerCase();
+
+
+                return (
+                    <li key={category}>
+                        <label>
+                            {categoryLabel}
+                            <input id={category} name={category} type="checkbox"/>
+                        </label>
+                    </li>
+                )
+            })}
+
+            <button type="submit">Filter by Selected</button>
+        </form>
+    )
+
+}
+
+function useCategories() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            const res = await axios.get("http://localhost:8080/products/categories")
+            setCategories(res.data);
+        }
+        load();
+    }, []);
+
+    return categories;
+}
+
+function handleSubmit() {
+    
 }
